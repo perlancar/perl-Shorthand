41be42d4428b420375c4151ebfbe3da72599b6e6
perlancar (on pc-office) <perlancar@gmail.com>
2018-04-18 22:01:36 +0700

    Initial commit

---
+++ lib/Shorthand.pm
@@ -0,0 +1,11 @@
+package Shorthand;
+
+1;
+# ABSTRACT: Shorthand
+
+=head1 FAQ
+
+=head2 Why not pragma?
+
+Naming your module as a pragma might indicate that the effect of your module can
+be scoped lexically.
