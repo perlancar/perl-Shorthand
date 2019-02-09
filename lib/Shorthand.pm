7bbbbc822e4664ef1501a5307f982a692de99b1e
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-02-10 06:04:13 +0700

    Release v0.001

--- lib/Shorthand.pm
+++ lib/Shorthand.pm
@@ -1,7 +1,27 @@
 package Shorthand;
 
+# DATE
+# VERSION
+
 1;
 # ABSTRACT: Shorthand
+
+=head1 DESCRIPTION
+
+B<Shorthand> is namespace reserved for modules that perform a shorthand for
+something. For example:
+
+ use Shorthand::Entropy::UseLocal;
+
+is a shorthand for:
+
+ use Data::Entropy;
+ use Data::Entropy::Source;
+ use Data::Entropy::RawSource::Local;
+
+ $Data::Entropy::entropy_source = Data::Entropy::Source->new(
+     Data::Entropy::RawSource::Local->new, "sysread");
+
 
 =head1 FAQ
 
